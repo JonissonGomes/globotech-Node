4cc669df1ae0007f4262aa77947efdcda7fccf28
JonissonGomes <joni_gomes@hotmail.com>
2022-02-23 22:39:27 -0300

    Criando desafio 1

---
+++ index.js
@@ -0,0 +1,27 @@
+// Importar AXIOS
+const axios = require('axios')
+const fs = require('fs');
+
+// criar a variavel para consumir a API
+const apiService = axios.create({
+    baseURL: 'https://rickandmortyapi.com/api'
+});
+
+// Criar função para capturar os dados da API
+async function getDataApi() {
+    const request = await apiService.get('/character')
+    createDocument(request.data)
+}
+
+getDataApi()
+
+// Criar a função para gravar os arquivos no formato JSON
+function createDocument(arquivo) {
+    let dadosRecebidos = arquivo
+    let nomeDoArquivo = "PersonagensRick-Morty"
+    fs.writeFile( nomeDoArquivo, JSON.stringify(dadosRecebidos), "utf-8", (err) => {
+        if(err) throw err;
+    console.log( nomeDoArquivo, ' foi criado com sucesso')
+     })
+}
+
